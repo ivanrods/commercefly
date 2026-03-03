5b1151e53e970444715f647b3d0ffbd5d1a31c9d
Ivan <contaivanrodrigues@gmail.com>
2026-03-03 14:55:53 -0300

    feat: added clerk webhook to user

---
+++ src/app/api/webhooks/clerk/route.ts
@@ -0,0 +1,54 @@
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
+import { NextRequest } from "next/server";
+import prisma from "../../../../lib/prisma";
+
+export async function POST(req: NextRequest) {
+  try {
+    const evt = await verifyWebhook(req);
+
+    const eventType = evt.type;
+
+    console.log(
+      `Received webhook with ID ${evt.data.id} and event type ${eventType}`,
+    );
+
+    if (eventType === "user.created") {
+      const { id, email_addresses, first_name, last_name } = evt.data;
+
+      await prisma.user.upsert({
+        where: { clerkId: id },
+        update: {},
+        create: {
+          clerkId: id,
+          email: email_addresses[0]?.email_address ?? "",
+          name: `${first_name ?? ""} ${last_name ?? ""}`.trim(),
+        },
+      });
+    }
+
+    if (eventType === "user.updated") {
+      const { id, email_addresses, first_name, last_name } = evt.data;
+
+      await prisma.user.update({
+        where: { clerkId: id },
+        data: {
+          email: email_addresses[0]?.email_address ?? "",
+          name: `${first_name ?? ""} ${last_name ?? ""}`.trim(),
+        },
+      });
+    }
+
+    if (eventType === "user.deleted") {
+      const { id } = evt.data;
+
+      await prisma.user.delete({
+        where: { clerkId: id },
+      });
+    }
+
+    return new Response("Webhook received", { status: 200 });
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
+  }
+}
