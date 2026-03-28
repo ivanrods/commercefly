4bc518f0009ca842757216f6cc9857f890ec76a0
Ivan <contaivanrodrigues@gmail.com>
2026-03-28 10:56:30 -0300

    refactor: slug category page styles

--- src/app/(public)/category/[slug]/page.tsx
+++ src/app/(public)/category/[slug]/page.tsx
@@ -1,5 +1,8 @@
 import { getCategoryBySlug } from "src/services/category-service";
 import { ProductCard } from "../../components/product-card";
+import { Button } from "src/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 type Props = {
   params: Promise<{
@@ -17,14 +20,41 @@ export default async function CategoryPage({ params }: Props) {
   }
 
   return (
-    <div className="max-w-6xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-6">{category.name}</h1>
+    <section className="w-full px-8 py-12">
+      <header className="mx-auto mb-12 ">
+        <div className="flex flex-col items-center justify-between gap-6 text-center md:flex-row md:text-start">
+          <div className="space-y-4">
+            <div className="inline-flex">
+              <span className="bg-secondary/80 text-secondary-foreground rounded-full px-4 py-1.5 text-sm font-medium">
+                Produtos da categoria
+              </span>
+            </div>
+            <div className="space-y-2">
+              <h2 className="text-foreground text-3xl font-bold tracking-tight md:text-4xl">
+                {category.name}
+              </h2>
+              <p className="text-muted-foreground max-w-2xl">
+                As melhores produtos em {category.name} para você!
+              </p>
+            </div>
+          </div>
+          <Link href="/">
+            {" "}
+            <Button className="cursor-pointer whitespace-nowrap" size="lg">
+              Voltar para a página inicial
+              <ArrowRight className="ms-2" />
+            </Button>
+          </Link>
+        </div>
+      </header>
 
-      <div className="grid items-center gap-4 grid-cols-1 md:grid-cols-3  lg:grid-cols-4 xl:grid-cols-5">
-        {category.products.map((product) => (
-          <ProductCard key={product.id} product={product} id={product.id} />
-        ))}
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:gap-6 xl:grid-cols-6">
+        {category.products.map((product) => {
+          return (
+            <ProductCard key={product.id} product={product} id={product.id} />
+          );
+        })}
       </div>
-    </div>
+    </section>
   );
 }
