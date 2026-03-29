0a4d094809f461d040e9851f8f5302c680acc7db
Ivan <contaivanrodrigues@gmail.com>
2026-03-28 21:26:13 -0300

    refactor: update product page styles

--- src/app/(public)/product/[id]/page.tsx
+++ src/app/(public)/product/[id]/page.tsx
@@ -1,7 +1,17 @@
+import { MoveRight, Store } from "lucide-react";
 import Image from "next/image";
+
+import Link from "next/link";
+import { Badge } from "src/components/ui/badge";
 import { Button } from "src/components/ui/button";
-import { Card, CardContent } from "src/components/ui/card";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "src/components/ui/carousel";
 import { formatCurrency } from "src/helpers/format-currency";
+import { cn } from "src/lib/utils";
 import { getProductById } from "src/services/product-service";
 
 type Props = {
@@ -20,39 +30,89 @@ export default async function ProductPage({ params }: Props) {
   }
 
   return (
-    <div className="max-w-7xl mx-auto my-32 ">
-      <Card className="border-none shadow-none">
-        <CardContent className="grid grid-cols-1 md:grid-cols-2 gap-10">
-          <div className="relative w-full aspect-square rounded-xl overflow-hidden bg-muted">
-            <Image
-              src={product.imageUrl}
-              alt={product.name}
-              fill
-              className="object-cover"
-              priority
-            />
-          </div>
-
-          <div className="flex flex-col gap-6">
-            <div>
-              <h1 className="text-3xl font-bold">{product.name}</h1>
-              <p className="text-muted-foreground mt-2">
+    <div>
+      <section className="@container mx-auto max-w-7xl md:mt-32">
+        <div className="grid grid-cols-1 gap-6 p-4 lg:grid-cols-2 lg:gap-8 lg:p-6 xl:grid-cols-3 xl:gap-12 xl:p-12">
+          <div className="flex flex-col justify-between gap-6 lg:gap-8">
+            <div className="space-y-2 lg:space-y-4">
+              <span className="text-sm font-semibold tracking-wide uppercase">
+                CommerceFly
+              </span>
+              <h2 className="text-xl font-bold tracking-tight text-balance lg:text-3xl">
+                {product.name}
+              </h2>
+              <p className="text-muted-foreground text-balance">
                 {product.description}
               </p>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <p className="text-3xl font-bold text-primary">
+              <p className="text-2xl font-bold tracking-tight">
                 {formatCurrency(product.price)}
               </p>
             </div>
 
-            <Button size="lg" className="w-full md:w-fit">
-              Comprar agora
-            </Button>
+            <div className="flex flex-wrap gap-4">
+              <div
+                key={product.imageUrl}
+                className={cn(
+                  "ring-offset-background size-16 lg:size-18 cursor-pointer overflow-hidden rounded-sm ring-offset-2 transition-all relative",
+                )}
+              >
+                <Image
+                  src={product.imageUrl}
+                  alt={product.name}
+                  fill
+                  className="object-cover"
+                  sizes="72px"
+                />
+              </div>
+            </div>
           </div>
-        </CardContent>
-      </Card>
+
+          <div className="row-span-2 row-start-1 aspect-square lg:col-start-2">
+            <Carousel className="size-full">
+              <CarouselContent>
+                <CarouselItem key={product.id}>
+                  <div className="relative w-full aspect-square">
+                    <Image
+                      src={product.imageUrl}
+                      alt={product.name}
+                      fill
+                      className="rounded-lg object-cover"
+                      sizes="100vw"
+                    />
+                  </div>
+                </CarouselItem>
+              </CarouselContent>
+            </Carousel>
+          </div>
+
+          <div className="flex flex-col gap-6 lg:gap-10">
+            {product.isFeatured && <Badge>Featured</Badge>}
+
+            <div className="space-y-2">
+              <h3 className="font-bold">Estoque disponível</h3>
+              <div className="flex flex-wrap gap-3">
+                Produtos: {product.stock}
+              </div>
+            </div>
+
+            <div className="space-y-2">
+              <h3 className="font-bold">Categoria</h3>
+              <div className="flex space-x-1">{product.category.name}</div>
+            </div>
+
+            <Button className=" cursor-pointer rounded-full" size="lg">
+              Add to Cart
+            </Button>
+            <Link href="/">
+              <Button variant="outline" className="w-full cursor-pointer">
+                <Store className="me-2 size-4" />
+                Continuar comprando
+                <MoveRight className="ms-2 size-4" />
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
