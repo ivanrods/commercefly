d588458a829fc03cd81b35df3e3f54bade753de4
Ivan <contaivanrodrigues@gmail.com>
2026-04-01 10:54:29 -0300

    feat: update sete-header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { SiteHeader } from "../components/header/site-header";
+import { SiteHeader } from "../components/site-header";
 import { ReactQueryProvider } from "src/providers/react-query-provider";
 import { BottomNav } from "src/components/bottom-nav";
 
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -6,24 +6,34 @@ import {
   UserButton,
 } from "@clerk/nextjs";
 
-import { ShoppingBag, ShoppingCart } from "lucide-react";
+import { ShoppingBag, ShoppingBasket, ShoppingCart } from "lucide-react";
 
-import { Separator } from "../ui/separator";
-import { Button } from "../ui/button";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
 import Link from "next/link";
 
 export function SiteHeader() {
   return (
     <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 py-2 lg:gap-2 lg:px-6">
-        <ShoppingBag />
-        <h1 className="text-base font-medium">CommerceFly</h1>
+        <Link href="/" className="flex gap-1">
+          <ShoppingBag />
+          <h1 className="text-base font-medium">CommerceFly</h1>
+        </Link>
+
         <div className="ml-auto flex items-center gap-2">
-          <Link href="/cart">
-            <Button variant="ghost">
-              <ShoppingCart />
-            </Button>
-          </Link>
+          <div className="hidden space-x-1 md:block">
+            <Link href="/orders" title="Compras">
+              <Button variant="ghost">
+                <ShoppingBasket />
+              </Button>
+            </Link>
+            <Link href="/cart" title="Carrinho">
+              <Button variant="outline">
+                <ShoppingCart />
+              </Button>
+            </Link>
+          </div>
 
           <Separator
             orientation="vertical"
