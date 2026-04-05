fc779eee1560e119ce29676bf605e2c859741322
Ivan <contaivanrodrigues@gmail.com>
2026-04-05 14:33:09 -0300

    feat: add create category route

--- src/app/api/categories/[slug]/route.ts
+++ src/app/api/categories/[slug]/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
+import { requireAdmin } from "src/lib/auth";
 
 interface Params {
   params: {
@@ -47,5 +48,56 @@ export async function GET(req: Request, { params }: Params) {
       error instanceof Error ? error.message : "Erro ao buscar categoria";
 
     return NextResponse.json({ error: message }, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await requireAdmin();
+
+    const body = await request.json();
+
+    const { name, slug, imageUrl } = body;
+
+    if (!name || !slug) {
+      return NextResponse.json(
+        { error: "Name and slug are required" },
+        { status: 400 },
+      );
+    }
+
+    const categoryExists = await prisma.category.findUnique({
+      where: { slug },
+    });
+
+    if (categoryExists) {
+      return NextResponse.json(
+        { error: "Category with this slug already exists" },
+        { status: 409 },
+      );
+    }
+
+    const category = await prisma.category.create({
+      data: {
+        name,
+        slug,
+        imageUrl: imageUrl ?? null,
+      },
+      select: {
+        id: true,
+        name: true,
+        slug: true,
+        imageUrl: true,
+      },
+    });
+
+    return NextResponse.json(category, { status: 201 });
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
   }
 }
